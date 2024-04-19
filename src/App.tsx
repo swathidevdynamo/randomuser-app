112c0e1749d2652c8afbaaaf52046e169fd5e830
swathi_munnangi73 <146412104+swathidevdynamo@users.noreply.github.com>
2024-04-19 20:08:16 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { GlobalStyles } from './app.styled';
+import { Route, Routes } from 'react-router-dom';
+import { Suspense, lazy } from 'react';
+import Home from './pages/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const LazyHomePage = lazy(() => import('./pages/Home'));
+
+const App = () => (
+  <div>
+    <GlobalStyles />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </Suspense>
+  </div>
+);
 
 export default App;
